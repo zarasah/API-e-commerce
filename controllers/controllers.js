54cc9019f11c5f995ad8ddffa5ad466848ec52b3
zarasah <zarinasah1989@gmail.com>
2023-03-21 11:19:14 +0400

    change cartItem's schema

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -84,22 +84,42 @@ function login(req, res) {
 
 function addToCart(req, res) {
     const token = req.headers.authorization;
+    const id = req.body.product_id;
     const decoded = jwt.decode(token);
     const { username } = decoded;
 
     db.get('SELECT * FROM users WHERE username = ?', [username], (error, row) => {
         if (error) {
-            res.send('Error');
+            res.send(error);
         } else {
             db.get('SELECT * FROM carts WHERE user_id = ?', [row.user_id], (error, row) => {
                 if (error) {
-                    res.send('Error');
+                    res.send(error);
                 } else {
-                    db.run('INSERT INTO CartItems (cart_id, product_id) VALUES (?, ?)', [row.cart_id, req.body.id], (error) => {
+                    const cartId = row.cart_id;
+                    
+                    db.get('SELECT * FROM CartItems WHERE cart_id = ? AND product_id = ?', [cartId, id], (error, row) => {
                         if (error) {
-                            res.send('Error');
-                        } else {
-                            res.send(JSON.stringify({ status:'Product added'}));
+                            res.send(error);
+                        } else {                            
+                            if (row) {
+                                let newCount = row.count + 1;
+
+                                db.run('UPDATE CartItems SET count = ? WHERE product_id = ?', [newCount, id], (error) => {
+                                    if (error) {
+                                        res.send(error);
+                                    } else {
+                                        res.send(JSON.stringify({ status:'Product added'}));
+                                    }
+                                })
+                            } else {
+                                db.run('INSERT INTO CartItems (cart_id, product_id, count) VALUES (?, ?, ?)', [cartId, id, 1], (error) => {
+                                    if (error) {
+                                    res.send(error);
+                                    }
+                                    res.send(JSON.stringify({ status:'Product added'}));
+                                })
+                            }
                         }
                     })
                 }
@@ -121,7 +141,7 @@ function showCart(req, res) {
                 if (error) {
                     res.send('Error2');
                 } else {
-                    db.all('SELECT productname, price FROM CartItems JOIN products ON products.product_id = CartItems.product_id WHERE cart_id = ?', [row.cart_id], (error, data) => {
+                    db.all('SELECT productname, price, count FROM CartItems JOIN products ON products.product_id = CartItems.product_id WHERE cart_id = ?', [row.cart_id], (error, data) => {
                         if (error) {
                             res.send('Error3');
                         } else {
--- models/cartItem_schema.js
+++ models/cartItem_schema.js
@@ -1,4 +1,4 @@
-const sql = 'CREATE TABLE IF NOT EXISTS CartItems (CartItem_id INTEGER PRIMARY KEY, cart_id INTEGER NOT NULL, product_id INTEGER NOT NULL, FOREIGN KEY (cart_id) REFERENCES carts(cart_id),FOREIGN KEY (product_id) REFERENCES products(product_id))';
+const sql = 'CREATE TABLE IF NOT EXISTS CartItems (CartItem_id INTEGER PRIMARY KEY, cart_id INTEGER NOT NULL, product_id INTEGER NOT NULL, count INTEGER, FOREIGN KEY (cart_id) REFERENCES carts(cart_id),FOREIGN KEY (product_id) REFERENCES products(product_id))';
 
 function createCartItemsTable(db) {
     db.run(sql);
